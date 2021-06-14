431a47464ed61fdd415bfd95a05ba1e6a80e9c71
Lindsay Schnell <lschnell8@gmail.com>
2021-06-13 18:54:14 -0600

    Build out constructor and toggleTurn method

--- src/game.js
+++ src/game.js
@@ -1,18 +1,25 @@
 class Game {
-  constructor(player1, player2) {
-  this.player1 = player1;
-  this.player2 = player2;
-  this.turn = player1;
-
+  constructor() {
+  this.player1 = new Player('Sir Squeaken');
+  this.player2 = new Player('Sir Floofintail');
+  this.turn = this.player1;
+  this.gameBoard = [null, null, null, null, null, null, null, null, null];
   }
   toggleTurn() {
     if(this.player1 === this.turn) {
-      this.turn = player2;
+      this.turn = this.player2;
     } else {
-      this.turn = player1
+      this.turn = this.player1;
     }
   }
-  resetGameBoard() {
-  setTimeOut() or setInterval()
-  }
+  // winAGame() {
+  //   for(var i = 0; i < this.gameBoard.length; i++) {
+  //     this.gameBoard[i] =
+  //   }
+  //   if(this.player1) {
+  //   player1.wins++
+  //   } else if(this.player2) {
+  //   player2.wins++
+  //   }
+  // }
 };
