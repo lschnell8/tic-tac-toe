478ec38e5d60ec6a45dba2e0115cb0ff32f61e79
Lindsay Schnell <lschnell8@gmail.com>
2021-06-14 00:26:42 -0600

    Replace property and add methods for calculating win conditions

--- src/game.js
+++ src/game.js
@@ -3,7 +3,8 @@ class Game {
   this.player1 = new Player('Sir Squeaken');
   this.player2 = new Player('Sir Floofintail');
   this.turn = this.player1;
-  this.gameBoard = [null, null, null, null, null, null, null, null, null];
+  this.boardProspects = [['b0', 'b1', 'b2'], ['b3', 'b4', 'b5'], ['b6', 'b7', 'b8'], ['b2', 'b4', 'b6'], ['b0', 'b4', 'b8'], ['b0', 'b3', 'b6'], ['b1', 'b4', 'b7'], ['b2', 'b5', 'b8']];
+
   }
   toggleTurn() {
     if(this.player1 === this.turn) {
@@ -12,14 +13,24 @@ class Game {
       this.turn = this.player1;
     }
   }
-  // winAGame() {
-  //   for(var i = 0; i < this.gameBoard.length; i++) {
-  //     this.gameBoard[i] =
-  //   }
-  //   if(this.player1) {
-  //   player1.wins++
-  //   } else if(this.player2) {
-  //   player2.wins++
-  //   }
-  // }
+  generateValue() {
+    for(var i = 0; i < this.boardProspects.length; i++) {
+      if(this.boardProspects[i][0] === event.target.id) {
+        this.boardProspects[i][0] = this.turn.id
+      } else if(this.boardProspects[i][1] === event.target.id) {
+        this.boardProspects[i][1] = this.turn.id
+      } else if(this.boardProspects[i][2] === event.target.id) {
+        this.boardProspects[i][2] = this.turn.id
+      }
+    }
+  }
+  checkForWinner() {
+    for(var i = 0; i < this.boardProspects.length; i++) {
+      if(this.boardProspects[i][0] === this.player1.id && this.boardProspects[i][1] === this.player1.id && this.boardProspects[i][2] === this.player1.id) {
+        this.player1.incrementWins();
+      } else if(this.boardProspects[i][0] === this.player2.id && this.boardProspects[i][1] === this.player2.id && this.boardProspects[i][2] === this.player2.id) {
+        this.player2.incrementWins();
+      }
+    }
+  }
 };
