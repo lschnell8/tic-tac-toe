2d5d37985a23e6ce44bd09186fffcd7e2e283590
Lindsay Schnell <lschnell8@gmail.com>
2021-06-15 02:59:58 -0600

    Remove pseudocode

--- src/player.js
+++ src/player.js
@@ -10,11 +10,8 @@ class Player {
   }
   saveWinsToStorage() {
     localStorage.setItem(`${this.id} storedWins`, JSON.stringify(this.wins))
-    // this.wins >> stringify >> save to local storage
   }
   retrieveWinsFromStorage() {
-    this.wins = JSON.parse(localStorage.getItem(`${this.id} storedWins`))
-    //reassigning this.wins to parsed local storage updated data model with localStorage then display this.wins from local localStorage
-    // retrieveWinsFromStorage should only be on page load and updates the data model so that we don't start from 0 each time.
+    this.wins = JSON.parse(localStorage.getItem(`${this.id} storedWins`)) || 0;
   }
 };
